fb589ccae8000c3df69683522049eb1dec5c9804
Andrej Čilag <andrej.chilag@uns.ac.rs>
2023-10-26 20:45:33 +0200

    Dodata inicijalizacija enkodera

--- Core/Lib/periferije/tajmer/tajmer.c
+++ Core/Lib/periferije/tajmer/tajmer.c
@@ -88,6 +88,53 @@ tim2_init ()
   TIM2->CR1 |= (0b1 << 0); // Uključivanje tajmera
 }
 
+static void
+tim3_init ()
+{
+  RCC->AHB1ENR |= (0b1 << 1); // Dozvola takta za GPIOB
+  RCC->APB1ENR |= (0b1 << 1); // Dozvola takta za TIM3
+
+  // Podešavanje pinova
+  uint8_t const KANAL_A = 4;
+  uint8_t const KANAL_B = 5;
+
+  // Podešavanje pinova da rade kao alternativna funkcija
+  GPIOB->MODER &= ~(0b11 << 2 * KANAL_A);
+  GPIOB->MODER &= ~(0b11 << 2 * KANAL_B);
+
+  GPIOB->MODER |= (0b10 << 2 * KANAL_A);
+  GPIOB->MODER |= (0b10 << 2 * KANAL_B);
+
+  // Odabir alternativne funkcije
+  GPIOB->AFR[0] &= ~(0b1111 << 4 * KANAL_A);
+  GPIOB->AFR[0] &= ~(0b1111 << 4 * KANAL_B);
+
+  uint8_t const ALTERNATIVNA_FUNKCIJA = 2;
+  GPIOB->AFR[0] |= (ALTERNATIVNA_FUNKCIJA << 4 * KANAL_A);
+  GPIOB->AFR[0] |= (ALTERNATIVNA_FUNKCIJA << 4 * KANAL_B);
+
+  // Podešavanje tajmera
+  TIM3->PSC = 0; // Nećemo da smanjujemo rezoluciju
+  TIM3->ARR = 0xFFFF; // Bitno je da najveći bit bude 1 zbog negativne brzine
+
+  // Podešavanje kvadraturnog dekodiranja
+  // Gledamo sve četiri promene signala
+  TIM3->SMCR &= ~(0b111 << 0);
+  TIM3->SMCR |= (0b011 << 0);
+
+  // Povezujemo kanale enkodera tajmera, sa kanalom samog tajmera
+  TIM3->CCMR1 &= ~(0b11 << 0);
+  TIM3->CCMR1 |= (0b01 << 0);
+  TIM3->CCMR1 &= ~(0b11 << 8);
+  TIM3->CCMR1 |= (0b01 << 8);
+
+  // Invertovanje kanala (odnosno neinvertovanje kanala)
+  TIM3->CCER &= ~((0b1 << 1) | (0b1 << 3)); // neinvertovanje kanala 1
+  TIM3->CCER &= ~((0b1 << 5) | (0b1 << 7)); // neinvertovanje kanala 2
+
+  TIM3->CR1 |= (0b1 << 0); // Uključivanje tajmera
+}
+
 void
 TIM2_IRQHandler ()
 {
